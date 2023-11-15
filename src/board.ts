94a214b26317daff5a38f782d973153e9e6e4c2e
Nguyen Vu <phucnguyenvu0203@gmail.com>
2023-11-14 22:07:07 -0800

    D3c complete

--- src/board.ts
+++ src/board.ts
@@ -1,4 +1,4 @@
-interface Cell {
+export interface Cell {
   x: number;
   y: number;
 }
@@ -16,9 +16,9 @@ export class Board {
   }
 
   getGridCell(x: number, y: number): Cell {
-    const i = x;
-    const j = y;
-    const key = `${i.toFixed(4)}_${j.toFixed(4)}`;
+    const i = Math.round(x * 10000);
+    const j = Math.round(y * 10000);
+    const key = `${i}_${j}`;
     if (this.oldCells.has(key)) {
       return this.oldCells.get(key)!;
     } else {
@@ -35,28 +35,38 @@ export class Board {
 
 export class Cache {
   coinList: Coins[];
+  cell: Cell;
   description: string;
 
   constructor(cell: Cell) {
     this.description = `${cell.x}_${cell.y}`;
+    this.cell = cell;
     this.coinList = [];
   }
 
-  addCoin(cell: Cell) {
-    const curSerial: number =
+  addCoin() {
+    const Serial: number =
       this.coinList.length > 0
         ? this.coinList[this.coinList.length - 1].serial + 1
         : 0;
-    this.coinList.push({ coord: cell, serial: curSerial });
+    this.coinList.push({ coord: this.cell, serial: Serial });
   }
 
   format(): string[] {
     const res: string[] = [];
     this.coinList.map((coin) => {
       res.push(
-        `${coin.coord.x.toFixed(4)}:${coin.coord.y.toFixed(4)}#${coin.serial}`
+        `${coin.coord.x.toFixed(0)}:${coin.coord.y.toFixed(0)}#${coin.serial}`
       );
     });
     return res;
+  }
+
+  toMomento(): string {
+    return this.description;
+  }
+
+  fromMomento(momento: string) {
+    this.description = momento;
   }
 }
